18cbca6d8b8b14db87df9540943c4d0318ccc8ac
balintnb <61189377+balintnb@users.noreply.github.com>
2021-02-24 12:35:51 +0100

    Add files via upload

--- js/multi-location.js
+++ js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 47.4733
+                lng: 19.0379
             },
         },
     ];
@@ -23,19 +23,19 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '5 5 5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '2 2 2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
+        scale: '8 8 8',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
