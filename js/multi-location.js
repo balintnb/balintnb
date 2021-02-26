4fe410f3ad8b01506f367b121dd1bca700e353da
balintnb <61189377+balintnb@users.noreply.github.com>
2021-02-26 08:48:50 +0100

    Add files via upload

--- js/multi-location.js
+++ js/multi-location.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    button.innerText = 'gomb';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
